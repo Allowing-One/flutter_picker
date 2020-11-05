1479d62fbba3d6ec8347f153365f39fc1ea5076c
Allowing-One <69533041+Allowing-One@users.noreply.github.com>
2020-11-06 02:39:51 +0900

    Update Picker.dart

--- lib/Picker.dart
+++ lib/Picker.dart
@@ -1046,18 +1046,18 @@ class DateTimePickerAdapter extends PickerAdapter<DateTime> {
   final List<int> customColumnType;
 
   static const List<String> MonthsList_EN = const [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월"
   ];
 
   static const List<String> MonthsList_EN_L = const [
